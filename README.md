3d836c4186e42e48188f8e4622ec3cff7b1ce542
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-11-19 01:27:57 +0100

    Started basic structure for daemon

---
+++ README.md
@@ -0,0 +1,11 @@
+# ShareIt! daemon
+
+Jesús Leganés Combarro "Piranna" - [piranna@gmail.com]
+
+This folder contains a ShareIt! client developed as a system daemon on Node.js.
+It's purpose is to run stand-alone on head-less machines so it can be queried
+from remote machines or properly configured ShareIt! clients (maybe just one
+served by the own daemon) allowing the same functionality but working on an
+unnatended, always connected machine.
+
+It's main purpose is to serve as a backend to be controlled from mobile phones.
---
+++ app.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+# ShareIt! daemon
+#
+# Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
+#
+# This folder contains a ShareIt! client developed as a system daemon on Node.js
+# It's purpose is to run stand-alone on head-less machines so it can be queried
+#from remote machines or properly configured ShareIt! clients (maybe just one
+# served by the own daemon) allowing the same functionality but working on an
+# unnatended, always connected machine.
+#
+# It's main purpose is to serve as a backend to be controlled from mobile phones
+
+var webp2p = require('../js/webp2p')
